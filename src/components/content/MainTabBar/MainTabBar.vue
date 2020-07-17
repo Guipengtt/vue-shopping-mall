9ea98911d7f511fc9a17eb2c1969ac2966fb3a90
Guipengtt <xgp980303@gmail.com>
2020-07-17 02:15:15 -0500

    update common modules

--- src/components/content/MainTabBar/MainTabBar.vue
+++ src/components/content/MainTabBar/MainTabBar.vue
@@ -1,6 +1,6 @@
 <template>
   <tab-bar>
-    <tab-bar-item path="/home" active-color="red">
+    <tab-bar-item path="/home">
       <img slot="item-icon" src="../../../assets/img/tabbar/home.svg" />
       <img
         slot="item-icon-active"
@@ -8,7 +8,7 @@
       />
       <div slot="item-text">首页</div>
     </tab-bar-item>
-    <tab-bar-item path="/category" active-color="red">
+    <tab-bar-item path="/category">
       <img slot="item-icon" src="../../../assets/img/tabbar/category.svg" />
       <img
         slot="item-icon-active"
@@ -16,7 +16,7 @@
       />
       <div slot="item-text">分类</div>
     </tab-bar-item>
-    <tab-bar-item path="/shopcart" active-color="red">
+    <tab-bar-item path="/shopcart">
       <img slot="item-icon" src="../../../assets/img/tabbar/cart.svg" />
       <img
         slot="item-icon-active"
@@ -24,7 +24,7 @@
       />
       <div slot="item-text">购物车</div>
     </tab-bar-item>
-    <tab-bar-item path="/profile" active-color="red">
+    <tab-bar-item path="/profile">
       <img slot="item-icon" src="../../../assets/img/tabbar/profile.svg" />
       <img
         slot="item-icon-active"
