8cdbd6e8ba5588457c636068322c3e06b1ca3339
Guipengtt <xgp980303@gmail.com>
2020-07-18 04:38:41 -0500

    Home page request data

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -9,11 +9,25 @@
 
 <script>
 import NavBar from "@/components/common/navBar/NavBar";
+import { getHomeMultiData } from "@/network/home";
 
 export default {
   name: "Home",
   components: {
     NavBar
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: []
+    };
+  },
+  created() {
+    getHomeMultiData().then(res => {
+      console.log(res);
+      this.banners = res.data.data.banner;
+      this.recommends = res.data.data.recommend;
+    });
   }
 };
 </script>
