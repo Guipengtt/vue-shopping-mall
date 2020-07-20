4eae903ccc8986df82c36a2ded8817f2468026c6
Guipengtt <xgp980303@gmail.com>
2020-07-20 07:06:40 -0500

    Update home page

---
+++ src/views/home/FeatureVIew.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="feature">
+    <a href="https://act.mogujie.com/zzlx67">
+      <img src="@/assets/img/home/recommend_bg.jpg" alt="Fetures" />
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FeatureView"
+};
+</script>
+
+<style scoped>
+/* .freature img {
+  width: 100%;
+} */
+
+.feature {
+  display: flex;
+  margin-top: 10px;
+  font-size: 14px;
+  padding-bottom: 30px;
+  border-bottom: 10px solid #eee;
+}
+
+.feature-item {
+  flex: 1;
+  text-align: center;
+}
+
+.feature img {
+  width: 100%;
+  margin-bottom: 10px;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,19 +4,30 @@
       <div slot="center">购物街</div>
     </nav-bar>
     <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-v-iew></feature-v-iew>
+    <tab-control :titles="['流行', '新款', '精选']"></tab-control>
   </div>
 </template>
 
 <script>
 import NavBar from "@/components/common/navBar/NavBar";
-import { getHomeMultiData } from "@/network/home";
+import TabControl from "@/components/content/tabControl/TabControl";
+
 import HomeSwiper from "@/views/home/childComps/HomeSwiper";
+import RecommendView from "@/views/home/childComps/RecommendView";
+import FeatureVIew from "@/views/home/FeatureVIew";
+
+import { getHomeMultiData } from "@/network/home";
 
 export default {
   name: "Home",
   components: {
     NavBar,
-    HomeSwiper
+    HomeSwiper,
+    RecommendView,
+    FeatureVIew,
+    TabControl
   },
   data() {
     return {
@@ -35,8 +46,16 @@ export default {
 </script>
 
 <style scoped>
+#home {
+  padding-top: 44px;
+}
 .home-nav {
   background-color: var(--color-tint);
   color: white;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9px;
 }
 </style>
