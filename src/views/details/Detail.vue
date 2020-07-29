d6be60aa3dcfa8ccde63d045645c4fa0cfa72c75
Guipengtt <xgp980303@gmail.com>
2020-07-29 02:36:15 -0500

    Update detail page: goods params

--- src/views/details/Detail.vue
+++ src/views/details/Detail.vue
@@ -1,9 +1,14 @@
 <template>
   <div id="detail">
-    <detail-nav-bar></detail-nav-bar>
-    <detail-swaper :top-images="topImages"></detail-swaper>
-    <detail-base-info :goods="goods"></detail-base-info>
-    <detail-shop-info :shop="shop"></detail-shop-info>
+    <detail-nav-bar class="detail-nav"></detail-nav-bar>
+    <scroll class="content" ref="scroll">
+      <detail-swaper :top-images="topImages"></detail-swaper>
+      <detail-base-info :goods="goods"></detail-base-info>
+      <detail-shop-info :shop="shop"></detail-shop-info>
+      <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"></detail-goods-info>
+      <detail-param-info :paramInfo="goodsParam"></detail-param-info>
+      <detail-comment-info :commentInfo="commentInfo"></detail-comment-info>
+    </scroll>
   </div>
 </template>
 
@@ -12,8 +17,13 @@ import DetailNavBar from "@/views/details/childComps/DetailNavBar";
 import DetailSwaper from "@/views/details/childComps/DetailSwaper";
 import DetailBaseInfo from "@/views/details/childComps/DetailBaseInfo";
 import DetailShopInfo from "@/views/details/childComps/DetailShopInfo";
+import DetailGoodsInfo from "@/views/details/childComps/DetailGoodsInfo";
+import DetailParamInfo from "@/views/details/childComps/DetailParamInfo";
+import DetailCommentInfo from "@/views/details/DetailCommentInfo";
 
-import { getDetails, Goods, Shop } from "@/network/detail";
+import Scroll from "@/components/common/scroll/Scroll";
+
+import { getDetails, Goods, Shop, GoodsParam } from "@/network/detail";
 
 export default {
   name: "Detail",
@@ -23,6 +33,9 @@ export default {
       topImages: [],
       goods: {},
       shop: {},
+      detailInfo: {},
+      goodsParam: {},
+      commentInfo: {},
     };
   },
   components: {
@@ -30,6 +43,10 @@ export default {
     DetailSwaper,
     DetailBaseInfo,
     DetailShopInfo,
+    DetailGoodsInfo,
+    DetailParamInfo,
+    DetailCommentInfo,
+    Scroll,
   },
   created() {
     // 得到商品iid
@@ -51,10 +68,46 @@ export default {
 
       // 3. 获取店铺信息
       this.shop = new Shop(data.shopInfo);
+
+      // 4. 商品详情数据
+      this.detailInfo = data.detailInfo;
+
+      // 5. 获取参数信息
+      this.goodsParam = new GoodsParam(
+        data.itemParams.info,
+        data.itemParams.rule
+      );
+
+      // 6. 获取评论信息
+      if (data.rate.cRate != 0) {
+        this.commentInfo = data.rate.list[0];
+      }
     });
+  },
+
+  methods: {
+    imageLoad() {
+      this.$refs.scroll.refresh();
+    },
   },
 };
 </script>
 
 <style scoped>
+#detail {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+  height: 100vh;
+}
+
+.content {
+  height: calc(100% - 44px);
+}
+
+.detail-nav {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+}
 </style>
